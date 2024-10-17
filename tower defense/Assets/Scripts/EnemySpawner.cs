ebd02d413e1c6e254417724a93a0f3f7dab98493
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-17 08:22:44 -0300

    Metodo EnemiesPerWave

--- tower defense/Assets/Scripts/EnemySpawner.cs
+++ tower defense/Assets/Scripts/EnemySpawner.cs
@@ -8,14 +8,22 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int baseEnemies = 8;
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5;
+    [SerializeField] private float difficultyscalingFactor = 0.75f;
 
     private int currenteWave = 1;
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
 
-    private void Start()
+    private void StartWave()
     {
-        enemiesLeftToSpawn = baseEnemies;
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave();
+    }
+
+    private int EnemiesPerWave()
+    {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currenteWave, difficultyscalingFactor));
     }
 }
