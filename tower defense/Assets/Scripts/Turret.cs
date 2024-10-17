27c09ce1ab78e5392bcadefa1466414097635eb7
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-17 15:31:04 -0300

    metodo  RotateTowardsTarget

--- tower defense/Assets/Scripts/Turret.cs
+++ tower defense/Assets/Scripts/Turret.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class Turret : MonoBehaviour
 {
@@ -17,13 +18,32 @@ public class Turret : MonoBehaviour
         if (target == null)
         {
             FindTarget();
+            return;
         }
+
+        RotateTowardsTarget();
+
     }
 
     private void FindTarget()
     {
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2) transform.position, 0f, enemyMask);
+        if(hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
     }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = MathF.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        transform.rotation = targetRotation;
+    }
+
+
 
     private void OnDrawGizmosSelected()
     {
