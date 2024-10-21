8df28926bd4104f02f181cf07441cb46d41948ba
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-21 13:32:25 -0300

    Declaração das primeiras variaveis

---
+++ tower defense/Assets/Scripts/Ploat.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ploat : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sr;
+    private GameObject tower;
+}
