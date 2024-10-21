4419e81119bb1c7af6aebe7acf974b7b4453dd51
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-21 14:18:45 -0300

    metodo Start

--- tower defense/Assets/Scripts/LevelManager.cs
+++ tower defense/Assets/Scripts/LevelManager.cs
@@ -8,9 +8,16 @@ public static LevelManager instance;
     public Transform[] path;
     public Transform startPoint;
 
+    public int currency;
+
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        currency = 100;
     }
 }
