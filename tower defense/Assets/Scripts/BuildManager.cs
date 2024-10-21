7560571f50b32e5512daab2f5cd7687a7ac8e0b4
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-21 13:25:35 -0300

    primeiras variaveis

--- tower defense/Assets/Scripts/BuildManager.cs
+++ tower defense/Assets/Scripts/BuildManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class BuildManager : MonoBehaviour
 {
 public static BuildManager instance;
+
+    [SerializeField] private GameObject[] towerPrefabs;
+
+    private int selectedTower = 0;
     private void Awake()
     {
         instance = this;
