ca0edcc9b0940f3483137b064c07e8ddb791d8f2
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-23 14:39:11 -0300

    Comentarios EnemyMovement

--- tower defense/Assets/Scripts/EnemyMoviment.cs
+++ tower defense/Assets/Scripts/EnemyMoviment.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Gerencia o comportamento de movimentação dos inimigos, permitindo que eles sigam um caminho pré-definido em direção ao objetivo do jogo 
 public class EnemyMoviment : MonoBehaviour
 {
     [Header("References")]
+    // Referência ao componente Rigidbody2D para movimentação
     [SerializeField] private Rigidbody2D rb;
     [Header("Attributres")]
+    // Velocidade de movimento do inimigo
     [SerializeField] private float moveSpeed = 2;
 
+    // Transform do próximo alvo que o inimigo deve seguir
     private Transform target;
+
+    // Índice para rastrear a posição atual no caminho
     private int pathIndex = 0;
 
+
+    //Inicializa o movimento do inimigo definindo seu primeiro alvo, garantindo que ele tenha um ponto de partida.
     private void Start()
     {
         target = LevelManager.instance.path[pathIndex];
     }
 
+    // Controla a lógica de movimentação, detectando quando o inimigo chega a um ponto do caminho e decide se deve avançar para o próximo ponto ou se deve ser destruído ao atingir o final do caminho.
     private void Update()
     {
         if(Vector2.Distance(target.position, transform.position) <= 0.1f)
@@ -36,6 +45,7 @@ public class EnemyMoviment : MonoBehaviour
         }
     }
 
+    //Aplica a física ao inimigo, movendo-o de forma suave e controlada em direção ao próximo alvo, utilizando a velocidade definida para simular um movimento realista.
     private void FixedUpdate()
     {
         Vector2 direction = (target.position - transform.position).normalized;
