1b7c620fbf16418a377a2e4135250a8c3920104a
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-23 14:31:56 -0300

    Comentarios Da Classe LevelManager e BuildManager

--- tower defense/Assets/Scripts/BuildManager.cs
+++ tower defense/Assets/Scripts/BuildManager.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
-public static BuildManager instance;
+    // Singleton: Permite que a classe BuildManager seja acessível globalmente por meio da variável 'instance'
+    public static BuildManager instance;
 
-   // [SerializeField] private GameObject[] towerPrefabs;
+    // Array de torres, definido no Inspector do Unity 
     [SerializeField] private Tower[] towers;
 
+
+    // Índice da torre atualmente selecionada
     private int selectedTower = 0;
+
+    // Awake: Inicializa a instância Singleton quando o objeto é ativado
     private void Awake()
     {
         instance = this;
     }
 
+
+    // Retorna a torre atualmente selecionada com base no índice armazenado
     public Tower GetSelectedTower()
     {
         return towers[selectedTower];
     }
 
+    // Define a torre selecionada, alterando o valor do índice de 'selectedTower'
     public void SetSelectedTower(int _selectedTower)
     {
         selectedTower = _selectedTower;
--- tower defense/Assets/Scripts/LevelManager.cs
+++ tower defense/Assets/Scripts/LevelManager.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-public static LevelManager instance;
+    // Singleton: Permite acesso global à única instância de LevelManager
+    public static LevelManager instance;
+
+    // Array de pontos que compõem o caminho (path) que os inimigos seguirão
     public Transform[] path;
+
+    // Ponto inicial de onde os inimigos começam no nível
     public Transform startPoint;
 
+    // Moeda do jogador, utilizada para realizar compras ou ações no jogo
     public int currency;
 
-
+    // Awake: Inicializa a instância Singleton quando o objeto é ativado
     private void Awake()
     {
         instance = this;
     }
 
+    // Start: Define a quantidade inicial de moeda para o jogador
     private void Start()
     {
         currency = 250;
     }
 
+    // Aumenta a quantidade de moeda do jogador
     public void IncreaseCurrency(int amount)
     {
         currency += amount;
     }
 
+    // Tenta gastar moeda. Retorna true se houver saldo suficiente, caso contrário, retorna false
     public bool SpendCurrency(int amount)
     {
         if (amount <= currency)
