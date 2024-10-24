5ce7e4728c781292b3a3a815f3c57f60dd70ffef
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-24 13:29:06 -0300

    Comentarios script Health

--- tower defense/Assets/Scripts/Health.cs
+++ tower defense/Assets/Scripts/Health.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Gerencia a saúde dos inimigos no jogo, controlando quando devem ser destruídos e recompensando o jogador por eliminá-los.
 public class Health : MonoBehaviour
 {
+    // Pontos de vida do inimigo. Se chegar a 0, ele é destruído.
     [SerializeField] public int hitPoints = 2 ;
+    // Valor em moeda que o jogador recebe ao destruir o inimigo
     [SerializeField] private int currencyWorth = 50;
+    // Indica se o inimigo já foi destruído para evitar múltiplas destruições
     private bool isDestroyed = false;
 
 
 
-
+    // Reduz os pontos de vida do inimigo e, se chegar a 0, invoca o evento de destruição, recompensa o jogador e remove o inimigo do jogo.
     public void TakeDamage(int dmg)
     {
         hitPoints -= dmg;
