ac58b9e84d52a55767a4fca6ad92dd84c6027779
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-24 13:38:42 -0300

    Comentarios Script Menu

--- tower defense/Assets/Scripts/Menu.cs
+++ tower defense/Assets/Scripts/Menu.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor;
 
+
+//Gerencia a interface do menu relacionada à exibição da moeda do jogador.
 public class Menu : MonoBehaviour
 {
+    // Exibe o valor atual da moeda do jogador na interface
     [SerializeField] TextMeshProUGUI currencyUI;
 
+    // Atualiza o texto da moeda na interface a cada quadro renderizado
     private void OnGUI()
     {
         currencyUI.text = LevelManager.instance.currency.ToString();
     }
 
-    public void SetSelected()
-    {
-
-    }
 }
