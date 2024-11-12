e616e42e72213bee15b5a30cd88b6f165639f503
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-11-12 13:56:14 -0300

    Fazer o Banner aparecer e desaparecer

--- tower defense/Assets/Scripts/Propaganda.cs
+++ tower defense/Assets/Scripts/Propaganda.cs
@@ -10,10 +10,36 @@ public class Propaganda : MonoBehaviour, IUnityAdsInitializationListener, IUnity
     public string bannerAndroid = "Banner_Android";
     public string interstitialAndroid = "Interstitial_Android";
     public string rewardedAndroid = "Rewarded_Android";
+    public float relogio;
+    public bool registro;
+
+    private void Update()
+    {
+        relogio += Time.deltaTime;
+        Contagem();
+    }
 
     private void Start()
     {
         Banner();
+    }
+
+    void Contagem()
+    {
+        if(relogio >= 10)
+        {
+      
+            Advertisement.Banner.Hide();
+            relogio = 0;
+            registro = true;
+        }
+
+        if (relogio >= 5 &&  registro)
+        {
+            Advertisement.Banner.Show(bannerAndroid);
+            relogio = 0;
+            registro = false;
+        }
     }
 
     public void Banner()
