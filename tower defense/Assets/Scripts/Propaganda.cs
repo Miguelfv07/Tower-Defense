9a82fc6f3bf7bb8e99ccf63f238640b258e20936
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-11-12 13:30:53 -0300

    Metodo Banner

--- tower defense/Assets/Scripts/Propaganda.cs
+++ tower defense/Assets/Scripts/Propaganda.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class Advertisement : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsShowListener
+public class Propaganda : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsShowListener
 {
 
     public string id = "5729647";
     public string bannerAndroid = "Banner_Android";
     public string interstitialAndroid = "Interstitial_Android";
     public string rewardedAndroid = "Rewarded_Android";
+
+  
+        public void Banner()
+        {
+            Advertisement.Initialize(id, true, this);
+            Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+            Advertisement.Banner.Show(bannerAndroid);
+        }
+
     public void OnInitializationComplete()
     {
         
