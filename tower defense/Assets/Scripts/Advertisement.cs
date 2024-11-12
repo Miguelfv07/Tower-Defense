5d95dcccd8519681129cce01faaaf8c866084875
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-11-12 13:27:54 -0300

    implementando a interface

---
+++ tower defense/Assets/Scripts/Advertisement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Advertisement : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsShowListener
+{
+    public void OnInitializationComplete()
+    {
+        
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+       
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+       
+    }
+
+
+}
